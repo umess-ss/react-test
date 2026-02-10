b505c4229bc5fcac024f0129a6e01835432c8cfc
Umesh Rajbanshi <ums.rbc07@gmail.com>
2026-02-10 08:06:44 -0500

    task reducer function and component in saperate folder

--- src/components/TaskApp.jsx
+++ src/components/TaskApp.jsx
@@ -1,26 +1,5 @@
+import {taskReducer, initialState} from '../reducers/taskReducer.jsx'
 import { useState, useReducer } from "react";
-
-
-const initialState = [
-    {id:1, text: 'Learn React Hooks', completed: false}
-]
-
-function taskReducer(state, action){
-    switch(action.type){
-        case 'ADD_TASK':
-            return [...state, {id: Date.now(), text: action.payload, completed: false} ];
-        
-        case 'TOGGLE_TASK':
-            return state.map(
-                task => task.id === action.id ? {...task, completed: !task.completed} : task);
-
-        case 'DELETE_TASK':
-            return state.filter(task=> task.id !== action.id);
-
-        default:
-            return state;
-    }
-}
 
 export default function TaskApp(){
     const [state, dispatch] = useReducer(taskReducer, initialState);
---
+++ src/reducers/taskReducer.jsx
@@ -0,0 +1,23 @@
+import { useState, useReducer } from "react";
+
+
+export const initialState = [
+    {id:1, text: 'Learn React Hooks', completed: false}
+]
+
+export function taskReducer(state, action){
+    switch(action.type){
+        case 'ADD_TASK':
+            return [...state, {id: Date.now(), text: action.payload, completed: false} ];
+        
+        case 'TOGGLE_TASK':
+            return state.map(
+                task => task.id === action.id ? {...task, completed: !task.completed} : task);
+
+        case 'DELETE_TASK':
+            return state.filter(task=> task.id !== action.id);
+
+        default:
+            return state;
+    }
+}
